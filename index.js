932a84bcf2436f31314c039469726dafb5192561
venkatsundeeps <venkatsundeeps@gmail.com>
2026-01-12 10:46:35 +0530

    Fix Render start issue

--- index.js
+++ index.js
@@ -51,4 +51,8 @@ app.post("/webhook", async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT, () => console.log("Server running"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
