d517f41845bfa192237999a70b52a8f469cc8686
venkatsundeeps <venkatsundeeps@gmail.com>
2026-01-12 10:50:20 +0530

    Remove package-lock and fix server listen

--- index.js
+++ index.js
@@ -53,6 +53,6 @@ app.post("/webhook", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
